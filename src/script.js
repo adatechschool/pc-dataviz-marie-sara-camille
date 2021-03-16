8f901d8032e4ff760f8b8d7dc184a79b13ff8b80
Sara Bevilacqua <sarabev6@gmail.com>
2021-03-16 16:29:40 +0100

    V4

--- src/script.js
+++ src/script.js
@@ -6,86 +6,86 @@ const gold = '#EA0';
 const garnet = '#C25';
 const eggplant = '#636';
 
-//Bonhomme à gauche
+//Femme gauche
 
-const illo = new Zdog.Illustration({
+const illustration = new Zdog.Illustration({
   element: '#zdog-canvas',
   zoom: 15,
 }); 
 
 // ----- model ----- //
-
-var head = new Zdog.Shape({
-  addTo: illo,
+var womanHead = new Zdog.Shape({
+  addTo: illustration,
   //On a mis stroke: 22 et ça a fait un carré
   stroke: 12,
   color: gold,
-  translate:{x:-15, y:10}
+  translate:{x:-25, y:10}
 }); 
 
-var eye = new Zdog.Ellipse({
-  addTo: head,
-  diameter: 2,
+
+var womanHair = new Zdog.Ellipse({
+  addTo: womanHead,
+  diameter: 14,
   quarters: 2,
   //position x et y de l'oeil gauche
-  translate: { x: -2, y: 1, z: 4.5 },
+  translate: { y: -0.5, z: 4.5 },
   //sens de rotation de l'oeil gauche
   rotate: { z: -TAU/4 },
   color: eggplant,
-  //épaisseur sourcils
-  stroke: 0.5,
-}); 
-//oeil droit
-eye.copy({
-  translate: { x: 2, y: 1, z: 4.5 },
-}); 
-// smile
-new Zdog.Ellipse({
-  addTo: head,
-  diameter: 3,
-  //Les 2/4 d'un rond:
+  stroke: 3,
+
+});
+
+
+var womanFringe = new Zdog.Ellipse({
+  addTo: womanHead,
+  diameter: 10,
   quarters: 2,
-  translate: { y: 2.5, z: 4.5 },
-  rotate: { z: TAU/4 },
-  //forme de la bouche:
+  translate: { y: -3, z: 3 },
+  rotate: { z: -TAU/4 },
   closed: true,
-  color: '#FED',
+  color: eggplant,
   stroke: 0.5,
   fill: true,
 });
- 
-//Bonhomme à droite
-// ----- model ----- //
 
-var head2 = new Zdog.Shape({
-  addTo: illo,
-  //On a mis stroke: 22 et ça a fait un carré
-  stroke: 12,
-  color: gold,
-  translate: { x:15, y:10},
-}); 
 
-var eye2 = new Zdog.Ellipse({
-  addTo: head2,
+var womanBun = new Zdog.Ellipse({
+  addTo: womanHead,
+  diameter: 6,
+  quarters: 2,
+  translate: { y: -8 },
+  rotate: { z: -TAU/4 },
+  //forme de la bouche:
+  closed: true,
+  color: eggplant,
+  stroke: 0.5,
+  fill: true,
+  
+});
+
+
+var womanLeftEye = new Zdog.Ellipse({
+  addTo: womanHead,
   diameter: 2,
   quarters: 2,
-  //position x et y de l'oeil gauche
   translate: { x: -2, y: 1, z: 4.5 },
-  //sens de rotation de l'oeil gauche
   rotate: { z: -TAU/4 },
   color: eggplant,
   //épaisseur sourcils
   stroke: 0.5,
+  
 }); 
-//oeil droit
-eye2.copy({
+
+
+var womanRightEye = womanLeftEye.copy({
   translate: { x: 2, y: 1, z: 4.5 },
-}); 
-// smile
-new Zdog.Ellipse({
-  addTo: head2,
+});
+
+
+var womanSmile = new Zdog.Ellipse({
+  addTo: womanHead,
   diameter: 3,
-  //Les 2/4 d'un rond:
   quarters: 2,
   translate: { y: 2.5, z: 4.5 },
   rotate: { z: TAU/4 },
@@ -94,11 +94,79 @@ new Zdog.Ellipse({
   color: '#FED',
   stroke: 0.5,
   fill: true,
+  
+});
+
+//Speech
+
+var womanBigBubble = new Zdog.RoundedRect({
+  addTo: illustration,
+  width: 20,
+  height: 10,
+  cornerRadius: 3,
+  translate: { x: -20, y: -13, z: 10 },
+  stroke: 0.5,
+  color: '#EA0',
+  dragRotate: true,
+});
+
+
+var womanSmallBubble1 = new Zdog.Ellipse({
+  addTo: illustration,
+  diameter: 2,
+  translate: { x: -18, y:-5, z: 10 },
+  stroke: 0.5,
+  color: '#EA0',
+});
+
+
+var womanSmallBubble2 = womanSmallBubble1.copy({
+  translate: { x: -19, y:-1, z: 10 },
+});
+
+//Bonhomme à droite
+// ----- model ----- //
+
+var manHead = womanHead.copy({
+  translate: { x:25, y:10},
+})
+
+var manLeftEye = womanLeftEye.copy({
+  addTo: manHead,
+  translate: { x: -2, y: 1, z: 4.5 },
+})
+
+
+var manRighteye = womanLeftEye.copy({
+  addTo: manHead,
+  translate: { x: 2, y: 1, z: 4.5 },
+}); 
+
+
+var manSmile = womanSmile.copy({
+  addTo: manHead,
+  translate: { y: 2.5, z: 4.5 },
+});
+  
+
+//Speech:
+var manBigBubble = womanBigBubble.copy({
+  translate: { x: 15, y: -10, z: 10 },
+});
+
+
+var manSmallBubble1 = womanSmallBubble1.copy({
+  translate: { x: 18, y:-2, z: 10 },
+});
+
+
+var manSmallBubble2 = womanSmallBubble1.copy({
+  translate: { x: 20, y:2, z: 10 },
 });
 
 //fonction d'affichage
 function animate() {
-  illo.updateRenderGraph();
+  illustration.updateRenderGraph();
   requestAnimationFrame( animate );
 }
 
